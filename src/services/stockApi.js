5e6a6ffff3fba5c4e0c5b765d3660928a6332bae
Jhon7289 <kyawkyaw231027@gmail.com>
2026-07-26 22:45:54 +0900

    Fix API routes

--- src/services/stockApi.js
+++ src/services/stockApi.js
@@ -1,24 +1,13 @@
-import axios from "axios";
-import { io } from "socket.io-client";
-
-const API_URL = import.meta.env.VITE_API_URL;
-
-const API = axios.create({
-  baseURL: API_URL,
-});
-
 export const getWatchlists = () =>
-  API.get("/watchlists");
+  API.get("/api/watchlists");
 
 export const saveWatchlists = (watchlists) =>
-  API.post("/watchlists", watchlists);
+  API.post("/api/watchlists", watchlists);
 
 export const searchStocks = (name) =>
-  API.get(`/search/${name}`);
+  API.get(`/api/search/${name}`);
 
 export const getHistory = (code, range = "1M") =>
-  API.get(`/history/${code}?range=${range}`);
+  API.get(`/api/history/${code}?range=${range}`);
 
-export const socket = io(
-  API_URL.replace("/api", "")
-);
+export const socket = io(API_URL);
